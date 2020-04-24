efe4799b944d8953bee8ec43eee8cc9c851d6987
Tamal Saha <tamal@appscode.com>
2020-04-24 03:21:48 -0700

    Add openapi v3 schema for values file (#27)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ apis/installer/v1alpha1/stash_postgres_types.go
@@ -0,0 +1,77 @@
+/*
+Copyright The Stash Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	ResourceKindStashPostgres = "StashPostgres"
+	ResourceStashPostgres     = "stashpostgres"
+	ResourceStashPostgreses   = "stashpostgreses"
+)
+
+// StashPostgres defines the schama for Stash Postgres Installer.
+
+// +genclient
+// +genclient:skipVerbs=updateStatus
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:path=stashpostgreses,singular=stashpostgres,categories={stash,appscode}
+type StashPostgres struct {
+	metav1.TypeMeta   `json:",inline,omitempty"`
+	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	Spec              StashPostgresSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
+}
+
+// StashPostgresSpec is the schema for Stash Postgres values file
+type StashPostgresSpec struct {
+	//+optional
+	NameOverride string `json:"nameOverride" protobuf:"bytes,1,opt,name=nameOverride"`
+	//+optional
+	FullnameOverride string          `json:"fullnameOverride" protobuf:"bytes,2,opt,name=fullnameOverride"`
+	Image            ImageRef        `json:"image" protobuf:"bytes,3,opt,name=image"`
+	Backup           PostgresBackup  `json:"backup" protobuf:"bytes,4,opt,name=backup"`
+	Restore          PostgresRestore `json:"restore" protobuf:"bytes,5,opt,name=restore"`
+}
+
+type ImageRef struct {
+	Registry   string `json:"registry" protobuf:"bytes,1,opt,name=registry"`
+	Repository string `json:"repository" protobuf:"bytes,2,opt,name=repository"`
+	Tag        string `json:"tag" protobuf:"bytes,3,opt,name=tag"`
+}
+
+type PostgresBackup struct {
+	Args string `json:"args" protobuf:"bytes,1,opt,name=args"`
+}
+
+type PostgresRestore struct {
+	Args string `json:"args" protobuf:"bytes,1,opt,name=args"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// StashPostgresList is a list of StashPostgress
+type StashPostgresList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	// Items is a list of StashPostgres CRD objects
+	Items []StashPostgres `json:"items,omitempty" protobuf:"bytes,2,rep,name=items"`
+}
--- charts/stash-postgres/templates/_helpers.tpl
+++ charts/stash-postgres/templates/_helpers.tpl
@@ -3,8 +3,8 @@
 Expand the name of the chart.
 */}}
 {{- define "stash-postgres.name" -}}
-{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
-{{- end -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
+{{- end }}
 
 {{/*
 Create a default fully qualified app name.
@@ -12,29 +12,41 @@ We truncate at 63 chars because some Kubernetes name fields are limited to this 
 If release name contains chart name it will be used as a full name.
 */}}
 {{- define "stash-postgres.fullname" -}}
-{{- if .Values.fullnameOverride -}}
-{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" -}}
-{{- else -}}
-{{- $name := default .Chart.Name .Values.nameOverride -}}
-{{- if contains $name .Release.Name -}}
-{{- .Release.Name | trunc 63 | trimSuffix "-" -}}
-{{- else -}}
-{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
-{{- end -}}
-{{- end -}}
-{{- end -}}
+{{- if .Values.fullnameOverride }}
+{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- $name := default .Chart.Name .Values.nameOverride }}
+{{- if contains $name .Release.Name }}
+{{- .Release.Name | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" }}
+{{- end }}
+{{- end }}
+{{- end }}
 
 {{/*
 Create chart name and version as used by the chart label.
 */}}
 {{- define "stash-postgres.chart" -}}
-{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" -}}
-{{- end -}}
+{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
+{{- end }}
 
-# default labels for posgres-stash resources
+{{/*
+Common labels
+*/}}
 {{- define "stash-postgres.labels" -}}
-app.kubernetes.io/name: {{ include "stash-postgres.name" . }}
 helm.sh/chart: {{ include "stash-postgres.chart" . }}
-app.kubernetes.io/instance: {{ .Release.Name }}
+{{ include "stash-postgres.selectorLabels" . }}
+{{- if .Chart.AppVersion }}
+app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
+{{- end }}
 app.kubernetes.io/managed-by: {{ .Release.Service }}
-{{- end -}}
+{{- end }}
+
+{{/*
+Selector labels
+*/}}
+{{- define "stash-postgres.selectorLabels" -}}
+app.kubernetes.io/name: {{ include "stash-postgres.name" . }}
+app.kubernetes.io/instance: {{ .Release.Name }}
+{{- end }}
